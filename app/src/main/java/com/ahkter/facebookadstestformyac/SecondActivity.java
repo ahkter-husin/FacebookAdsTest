0f2b7426588db85eabbf8105729b962842fd5695
ahkt <ahkterhusin@gmail.com>
2020-12-17 14:09:44 +0800

    Initial commit,

---
+++ app/src/main/java/com/ahkter/facebookadstestformyac/SecondActivity.java
@@ -0,0 +1,125 @@
+package com.ahkter.facebookadstestformyac;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.ads.*;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
+import com.facebook.ads.AudienceNetworkAds;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private AdView adView;
+    private final String TAG = MainActivity.class.getSimpleName();
+    private InterstitialAd interstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+
+        // Initialize the Audience Network SDK
+        AudienceNetworkAds.initialize(this);
+
+        adView = new AdView(this, "336957984244151_386862329253716", AdSize.BANNER_HEIGHT_50);
+
+        // Find the Ad Container
+        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
+
+        // Add the ad view to your activity layout
+        adContainer.addView(adView);
+
+        // Request an ad
+        adView.loadAd();
+///////////////////////Banner ended////////////////////////
+
+////////////////////////////////////
+        interstitialAd = new InterstitialAd(this, "336957984244151_386892359250713");
+        // Create listeners for the Interstitial Ad
+        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                interstitialAd.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        };
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd(
+                interstitialAd.buildLoadAdConfig()
+                        .withAdListener(interstitialAdListener)
+                        .build());
+    }
+    private void showAdWithDelay() {
+        /**
+         * Here is an example for displaying the ad with delay;
+         * Please do not copy the Handler into your project
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            // Check if interstitialAd has been loaded successfully
+            if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+                return;
+            }
+            // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+            if(interstitialAd.isAdInvalidated()) {
+                return;
+            }
+            // Show the ad
+            interstitialAd.show();
+        }, 1000 * 30); // Show the ad after 1 minutes
+    }
+
+    @Override
+    public void onBackPressed() {
+        interstitialAd.show();
+        super.onBackPressed();
+    }
+}
+
+
+
